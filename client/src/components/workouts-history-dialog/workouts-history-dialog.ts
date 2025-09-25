62e1bb7057fe08506946ae02c9f5e5751f3fdf43
Lucas B. <boinet1906@gmail.com>
2025-09-25 22:03:56 +0200

    feat(client): add delete button in workouts history dialog

--- client/src/components/workouts-history-dialog/workouts-history-dialog.ts
+++ client/src/components/workouts-history-dialog/workouts-history-dialog.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { MessageService } from 'primeng/api';
 import { DialogModule } from 'primeng/dialog';
+import { WorkoutService } from '../../services/workout.service';
 import { Workout } from '../../shared/types/Workout';
 
 @Component({
   selector: 'workouts-history-dialog',
   imports: [CommonModule, DialogModule],
+  providers: [MessageService],
   templateUrl: './workouts-history-dialog.html',
 })
 export class WorkoutsHistory {
@@ -13,7 +16,35 @@ export class WorkoutsHistory {
   @Input() open = false;
   @Output() openChange = new EventEmitter<boolean>();
 
+  private workoutService = inject(WorkoutService);
+  private messageService = inject(MessageService);
+
   handleVisibleChange(value: boolean) {
     this.openChange.emit(value);
+  }
+
+  removeWorkout(workoutId: string) {
+    this.workoutService.deleteWorkout(workoutId).subscribe({
+      next: () => {
+        this.workoutService.workouts.set(
+          this.workoutService.workouts().filter((w) => w._id !== workoutId),
+        );
+
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Workout',
+          detail: `Workout deleted successfully`,
+          life: 3000,
+        });
+      },
+      error: () => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Workout',
+          detail: `Something wrong happened while deleting this workout`,
+          life: 3000,
+        });
+      },
+    });
   }
 }
