b98ab049d300abeb16e71aa5cb2a122b8d670c99
Lucas B. <boinet1906@gmail.com>
2025-10-03 00:55:51 +0200

    feat(client): add filters on stats page

--- client/src/pages/stats/stats.ts
+++ client/src/pages/stats/stats.ts
@@ -1,20 +1,46 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SelectModule } from 'primeng/select';
 import { WorkoutChart } from '../../components/workout-chart/workout-chart';
+import { SessionService } from '../../services/sessions.service';
 import { WorkoutService } from '../../services/workout.service';
+import { Session } from '../../shared/types/Session';
 import { WorkoutStat } from '../../shared/types/Workout';
 
 @Component({
   templateUrl: './stats.html',
   selector: 'stats-page',
-  imports: [WorkoutChart],
+  imports: [WorkoutChart, FormsModule, SelectModule],
 })
 export class StatsPage implements OnInit {
   stats = signal<WorkoutStat[]>([]);
+  selectedStat = signal<WorkoutStat | undefined>(undefined);
+  selectedSession = signal<Session | undefined>(undefined);
+
+  filteredStats = computed(() => {
+    let statsToFiler = this.stats();
+
+    if (this.selectedStat()) {
+      statsToFiler = statsToFiler.filter(
+        (stat) => stat.exerciseName === this.selectedStat()?.exerciseName,
+      );
+    }
+
+    if (this.selectedSession()) {
+      statsToFiler = statsToFiler.filter((stat) =>
+        this.selectedSession()?.exercises.find((e) => e.name === stat.exerciseName),
+      );
+    }
+
+    return statsToFiler;
+  });
 
   private workoutService = inject(WorkoutService);
+  public sessionService = inject(SessionService);
 
   ngOnInit() {
     this.fetchStats();
+    this.fetchSessions();
   }
 
   fetchStats() {
@@ -26,5 +52,21 @@ export class StatsPage implements OnInit {
         console.error('Failed to fetch workout stats', err);
       },
     });
+  }
+
+  fetchSessions() {
+    this.sessionService.getSessions().subscribe({
+      next: (sessions) => {
+        this.sessionService.sessions.set(sessions);
+      },
+    });
+  }
+
+  handleSelectedStatsChange(stat: WorkoutStat) {
+    this.selectedStat.set(stat);
+  }
+
+  handleSelectSessionChange(session: Session) {
+    this.selectedSession.set(session);
   }
 }
